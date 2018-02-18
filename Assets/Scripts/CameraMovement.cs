afc6f1cc34f84eec510ad55bf80b60c5b58bd2f7
MuaazWahid <qubedude@gmail.com>
2018-02-18 11:10:04 -0800

    Moves the camera to the right at an appropriate speed

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    float CameraSpeed = 0.1f;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * CameraSpeed);
+	}
+}
