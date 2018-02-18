724b2e83031b9edb6bf8c737d16fc56c917f4818
Vikram Peddinti <vikram.peddinti@gmail.com>
2018-02-17 17:32:07 -0800

    Powerup Framework - Creation
    Basic Powerup Framework utilizing an Enum and some beautiful switch statements. 
    Also imported VSCode Plugin.

---
+++ Assets/Scripts/Powerups/PR_Generic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Basic PowerUp Class based of an Enumerator and Switch system
+/// Author: Vikram Peddinti February 2018
+/// </summary>
+public enum Powerup
+{
+	Speed = 0,
+	Destroy = 1
+}
+
+
+public class PR_Generic : MonoBehaviour {
+
+	public Powerup prtype;
+	//public Collider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		//collider = gameObject.GetComponent<Collider2D>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "Player")
+            ApplyEffects();
+    }
+
+	public void ApplyEffects(){
+		switch (prtype)
+		{	
+			//Add the powerup code in each case statement
+			case Powerup.Speed:{
+				Debug.Log("How Speedy?");
+				Destroy(gameObject);
+				break;
+			}
+			case Powerup.Destroy:{
+				Debug.Log("How Destructiony?");
+				Destroy(gameObject);
+				break;
+			}
+			default:{
+				break;
+			}
+		} 
+
+	}
+
+}
