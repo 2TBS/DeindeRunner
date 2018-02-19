69411ca6db03efef7230afd30993ac00d9a61ae0
ShrinikethNarayana <shrinikethNarayana@gmail.com>
2018-02-19 15:47:07 -0800

    Added camera acceleration
    Accelerates camera, 2nd derivative included.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,13 +8,18 @@ public class CameraMovement : MonoBehaviour {
 	public InputManager inputManager;
 
     float CameraSpeed = 0.05f;
+	float CameraAcceleration = .01f;
+	float Camera2ndDerivative = 0.99f;
 	// Use this for initialization
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.T))
 			isMoving = !isMoving;
-		if(isMoving)
-        	transform.Translate(Vector2.right * CameraSpeed);
+		if (isMoving) {
+			transform.Translate (Vector2.right * CameraSpeed);
+			CameraSpeed = CameraSpeed * (1 + CameraAcceleration);
+			CameraAcceleration = CameraAcceleration * Camera2ndDerivative;
+		}
 	}
 }
