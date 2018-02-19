ae27df5cd94edbf5bd073e38a36eb6d4fffad55d
Adarsh Iyer <adarsh.iyer17@gmail.com>
2018-02-19 13:11:27 -0800

    Added Control to Start and Stop Moving Camera (is a prefab)

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
+	private bool isMoving = false;
 
-    float CameraSpeed = 0.02f;
+	public InputManager inputManager;
+
+    float CameraSpeed = 0.05f;
 	// Use this for initialization
-	void Start () {
-        Debug.Log("");
-    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.right * CameraSpeed);
+		if(Input.GetKeyDown(KeyCode.T))
+			isMoving = !isMoving;
+		if(isMoving)
+        	transform.Translate(Vector2.right * CameraSpeed);
 	}
 }
