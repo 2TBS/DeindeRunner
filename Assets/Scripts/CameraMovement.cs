616cf8d0b55697d2ca66b5f3ac2357de19aeb470
Akash Basu <33015666+FugaX@users.noreply.github.com>
2018-02-20 15:32:20 -0800

    Change Camera Movement
    Slower Camera Movement.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,19 +7,18 @@ public class CameraMovement : MonoBehaviour {
 
 	public InputManager inputManager;
 
-    float CameraSpeed = 0.05f;
+    float CameraSpeed = 0.02f;
 	float CameraAcceleration = .01f;
 	float Camera2ndDerivative = 0.99f;
 	// Use this for initialization
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.T))
 			isMoving = !isMoving;
 		if (isMoving) {
 			transform.Translate (Vector2.right * CameraSpeed);
-			CameraSpeed = CameraSpeed * (1 + CameraAcceleration);
-			CameraAcceleration = CameraAcceleration * Camera2ndDerivative;
+
 		}
 	}
 }
