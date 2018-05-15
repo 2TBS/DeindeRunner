c2736ca45bbab5a16feda920b6850fb2ccecaff9
Akash Basu <33015666+FugaX@users.noreply.github.com>
2018-05-14 19:26:32 -0700

    add sniper and weapons

--- Assets/Scripts/ProjectileLifecycle.cs
+++ Assets/Scripts/ProjectileLifecycle.cs
@@ -9,15 +9,24 @@ public class ProjectileLifecycle : MonoBehaviour {
 
 	[SerializeField] private int damage = 1;
 	[SerializeField] private int snipedamage = 2;
+	[SerializeField] private int weapondamage = 3;
 
 
+    // method for checking for whether the projectiles have collided or not
 	void OnCollisionEnter2D (Collision2D collision) {
+		//this if statement is for regular projectiles/bullets 
 		if (collision.gameObject.tag == "Damageable")
 			collision.gameObject.GetComponent<Destructable> ().Damage (damage);
 		Destroy (gameObject);
+		// this else if statement is for the "sniper" powerup
         else if(collision.gameObejct.tag == "Damageable")
             collision.gameObejct.GetComponent<Destructable> ().Damage (snipedamage);
           Destroy (gameObject);
+        // this else if statement is for the "weapons" implemented in the game 
+	    else if(collision.gameObejct.tag == "Damageable")
+            collision.gameObejct.GetComponent<Destructable> ().Damage (weapondamage);
+          Destroy (gameObject);
+		  
 	}
     
 
