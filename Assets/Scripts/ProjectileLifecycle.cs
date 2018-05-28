67aca5a233996e937b1072087bee2df1b1ef8894
Ben Cuan <ben@bananiumlabs.com>
2018-05-27 19:17:24 -0700

    Projectile Lifecycle hotfix

--- Assets/Scripts/ProjectileLifecycle.cs
+++ Assets/Scripts/ProjectileLifecycle.cs
@@ -1,8 +1,9 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 ///<summary>Attach to projectiles to facilitate collisions.</summary>
+/// Contributors: Ben Cuan, Unknown (maybe Akash)?
 public class ProjectileLifecycle : MonoBehaviour {
 
 	// Update is called once per frame
@@ -10,7 +11,6 @@ public class ProjectileLifecycle : MonoBehaviour {
 	[SerializeField] private int damage = 1;
 	[SerializeField] private int snipedamage = 2;
 	[SerializeField] private int weapondamage = 3;
-
 
     // method for checking for whether the projectiles have collided or not
 	void OnCollisionEnter2D (Collision2D col) {
@@ -20,21 +20,14 @@ public class ProjectileLifecycle : MonoBehaviour {
 			Destroy (gameObject);
 		}
 		// this else if statement is for the "sniper" powerup
-        else if(col.gameObejct.tag == "Damageable") {
-            col.gameObejct.GetComponent<Destructable> ().Damage (snipedamage);
+        else if(col.gameObject.tag == "Damageable") {
+            col.gameObject.GetComponent<Destructable> ().Damage (snipedamage);
           	Destroy (gameObject);
 		}
         // this else if statement is for the "weapons" implemented in the game 
-	    else if(col.gameObejct.tag == "Damageable") {
-            col.gameObejct.GetComponent<Destructable> ().Damage (weapondamage);
+	    else if(col.gameObject.tag == "Damageable") {
+            col.gameObject.GetComponent<Destructable> ().Damage (weapondamage);
           	Destroy (gameObject);
-		}
-		  
+		}	  
 	}
-    
-
-
-
-
-
-}*/
+}
