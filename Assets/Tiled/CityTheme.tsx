3acdd2daf0a8d6f1e6790a7057eb9a6e39623675
Jeffery Yang <jefferyyang@Jefferys-MacBook-Pro.local>
2018-06-09 15:36:26 -0700

    City Level

---
+++ Assets/Tiled/CityTheme.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CityTheme" tilewidth="32" tileheight="32" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Concrete.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="ConcreteRoof.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Door Half.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="RadioRec.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Concrete Lamp.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="ConcreteWindow.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="BasicGroundTexture 1 - Copy (2).png"/>
+ </tile>
+</tileset>
